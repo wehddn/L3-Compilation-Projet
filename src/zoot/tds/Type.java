7317219bd6f629ee61e9e85b871320db8235eed4
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2022-02-11 19:52:19 +0100

    Ajout d'un Enum : - Ajout de l'enum Type - Mise à jour de la version

---
+++ src/zoot/tds/Type.java
@@ -0,0 +1,13 @@
+package zoot.tds;
+
+/**
+ * Représente le type d'un symbole
+ *
+ * @author Elhadji Moussa FAYE
+ * @version 1.1.1
+ * @since 1.1.1
+ * created on 11/02/2022
+ */
+public enum Type {
+    ENTIER, BOOLEEN
+}
