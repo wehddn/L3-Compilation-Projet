10c201cb54d32e641c729fee7556561fc1902898
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2022-02-19 20:50:16 +0100

    Ajout de classe: - Ajout de la classe Programme - Changement de version

---
+++ src/zoot/arbre/Programme.java
@@ -0,0 +1,49 @@
+package zoot.arbre;
+
+import zoot.arbre.instructions.Instruction;
+
+/**
+ * Represente l'Arbre abstrait général (le programme)
+ *
+ * @author Elhadji Moussa FAYE
+ * @version 1.4.2
+ * @since 1.4.2
+ * created on 19/02/2022
+ */
+public class Programme extends BlocDInstructions{
+
+    public Programme(int n, int m) {
+        super(n, m);
+    }
+
+    @Override
+    public String toMIPS() {
+        StringBuilder sb = new StringBuilder() ;
+        // Ecrit le début du programme mips
+        sb.append(".text\n" +
+                "main :\n" +
+                "# initialiser $s7 avec $sp\n" +
+                "\tmove $s7, $sp\n" +
+                "# réserver la place pour ");
+        sb.append(taillePile / 4).append(" variable");
+
+        if (taillePile > 4) // Ajoute un s à variable s'il y'a plusieurs variables
+            sb.append('s');
+
+        // L'instruction qui réserve la place dans la pile pour les variables
+        sb.append("\n\tadd $sp, $sp, ").append(-taillePile).append("\n");
+
+        sb.append(super.toMIPS()).append("\n");
+
+        // Ecrit la fin du programme mips (retour)
+        sb.append("end :\n" +
+                "\tli $v0, 10\n" +
+                "\tsyscall");
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Programme :\nTaille pile : " + taillePile + "\n" + super.toString();
+    }
+}
