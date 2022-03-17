cf1ac1e8f0d22f1bceec1340dd3fa027d4c2c8e5
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2022-03-17 17:31:54 +0100

    zoot2 : - Mise à jour de la grammaire - On peut acceder aux variables dans le main depuis une fonction - Les fonctions peuvent maintenant contenir juste un retourne - Renommage des fonctions pour eviter les conflits avec les fonctions internes (temporaire) on implémentera un factory plus tard - Changement de version

--- src/zoot/arbre/Fonction.java
+++ src/zoot/arbre/Fonction.java
@@ -8,7 +8,7 @@ import zoot.tds.*;
  * Classe qui représente les différentes instructions d'une fonctions du programme
  *
  * @author Nicolas GRAFF
- * @version 1.8.0
+ * @version 1.9.0
  * @since 1.8.0
  * created on 07/03/2022
  */
@@ -17,17 +17,19 @@ public class Fonction extends ArbreAbstrait{
     private EntreeFct entree;
     private SymboleFct symbole = null;
     private Retourne retour;
-    private BlocDInstructions instructions = null;
+    private BlocDInstructions instructions;
 
     public Fonction(EntreeFct e, Retourne r, int n, int m) {
         super(n, m);
         this.entree = e;
         this.retour = r;
+        this.instructions = new BlocDInstructions(0, 0);
     }
 
     @Override
     public void verifier() {
         instructions.verifier();
+        retour.verifier();
         this.symbole = (SymboleFct) Tds.getInstance().identifier(entree, noLigne, noColonne);
         Type retourType = retour.getType();
         Type symboleType = symbole.getType();
@@ -44,6 +46,7 @@ public class Fonction extends ArbreAbstrait{
     }
 
     public void setBlocDInstructions(BlocDInstructions b) {
-        instructions = b;
+        if(b != null)
+            instructions = b;
     }
 }
